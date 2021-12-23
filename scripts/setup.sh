7167ff5e0c6cb7178042b7b1ba1504a4b752b5ae
Lưu Hoàng Sơn <sonlh.cs.uit@gmail.com>
2021-12-23 11:10:33 +0700

    remove misc file

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,23 @@
 DIR=$(pwd)
 LOG_INF="logs/info.log"
 LOG_ERR="logs/error.log"
+MALT_PARSER="http://maltparser.org/dist/maltparser-1.9.2.tar.gz"
+MALT_EVAL="https://raw.githubusercontent.com/sonlhcsuit/dotfiles/main/MaltEval-dist.zip"
+MST_PARSER="https://www.seas.upenn.edu/~strctlrn/MSTParser/MSTParser.tar.gz"
+
 mkdir -p logs
 #echo $DIR
 #echo "DOWNLOADING MSTParser..."
-#wget https://www.seas.upenn.edu/~strctlrn/MSTParser/MSTParser.tar.gz $DIR
+#wget $DIR
 #gunzip MSTParser.tar.gz
 #tar -xvf MSTParser.tar
 #rm MSTParser.tar
 
 echo "DOWNLOADING MaltParser..."
-#wget http://maltparser.org/dist/maltparser-1.9.2.tar.gz $DIR
+#wget $MALT_MALT_PARSER $DIR
 #gunzip maltparser-1.9.2.tar.gz
 #tar -xvf maltparser-1.9.2.tar
-mv maltparser-1.9.2/ MaltParser/
+#mv maltparser-1.9.2/ MaltParser/
 
-#rm MSTParser.tar
+#wget $MALT_EVAL $DIR
+unzip -o MaltEval-dist.zip
